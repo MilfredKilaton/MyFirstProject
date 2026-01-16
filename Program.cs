223cfc7ee411e98df255074f590b587a104d69ba
Milfred Kilaton <milfred.kilaton@dbtc-cebu.edu.ph>
2026-01-16 11:06:22 +0800

    The final output for my task 1 project

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace TaskMaster
+{
+    //// Made by: Milfred Kilaton
+    struct TaskPriority
+    {
+        public int TaskPriorityLevel;
+    }
+    class UserTask
+    {
+        public string taskName;
+        public TaskPriority Priority;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TaskPriority taskPriority;
+            taskPriority.TaskPriorityLevel = 10;
+
+            UserTask userTask = new UserTask();
+            userTask.taskName = "Second Semester Task 1";
+            userTask.Priority = taskPriority;
+
+            Console.WriteLine($"You have now completed the: {userTask.taskName}");
+        }
+    }
+}
